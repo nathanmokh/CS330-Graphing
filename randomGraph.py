aaaa8fbf41f013f3f143e7270c16bacb50455650
nathanmokh <nathanmo@bu.edu>
2018-02-20 14:25:24 -0500

    done with random gen graph

--- randomGraph.py
+++ randomGraph.py
@@ -1,28 +1,44 @@
 import random
 
 def createGraph(n):
+    #For randomly generated edge weights
     graph = {}
     for node in range(n):
        graph[node] = []
     return graph
 
-def createConnections(graph):
-    for node in graph:
-        i = node
-        while i < len(graph):
-            graph[i].append([i + 1, random.random()])
-            i += 1
-    return graph
-
 def createConn2(graph):
+    #For randomly generated edge weights
     nodeCounter = 0
     while (nodeCounter < len(graph)):
         j = nodeCounter + 1
         while j < len(graph):
-            x = round(random.random(), 1)
+            x = random.random()
             graph[nodeCounter].append([j, x])
             graph[j].append([nodeCounter, x])
             j += 1
         nodeCounter += 1
     return graph
+
+def createGraphDistance(n):
+    #For graph and plane
+    graph = {}
+    for node in range(n):
+       graph[node] = []
+    return graph
         
+def createConnDistance(graph):
+    #For graph and plane
+    nodeCounter = 0
+    while (nodeCounter < len(graph)):
+        j = nodeCounter + 1
+        while j < len(graph):
+            x = random.random()
+            y = random.random()
+            distance = 
+            graph[nodeCounter].append([j, x])
+            graph[j].append([nodeCounter, x])
+            j += 1
+        nodeCounter += 1
+    return graph
+
