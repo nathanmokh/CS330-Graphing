01f5e39289e5f1337f4c706310ba7c4254b68d48
nathanmokh <nathanmo@bu.edu>
2018-02-21 21:26:30 -0500

    can generate both graphs now

--- randomGraph.py
+++ randomGraph.py
@@ -24,7 +24,7 @@ def createGraphDistance(n):
     #For graph and plane
     graph = {}
     for node in range(n):
-       graph[node] = []
+       graph[node] = [random.randint(0,50), random.randint(0,50)]
     return graph
         
 def createConnDistance(graph):
@@ -33,11 +33,20 @@ def createConnDistance(graph):
     while (nodeCounter < len(graph)):
         j = nodeCounter + 1
         while j < len(graph):
-            x = random.random()
-            y = random.random()
-            distance = 
-            graph[nodeCounter].append([j, x])
-            graph[j].append([nodeCounter, x])
+            x_i = (graph[nodeCounter])[0]
+            y_i = (graph[nodeCounter])[1]
+            x_j = (graph[j])[0]
+            y_j = (graph[j])[1]
+            x_dis = abs(x_i - x_j)**2
+            y_dis = abs(y_i - y_j)**2
+            distance = (x_dis + y_dis)**(0.5)
+            #graph[nodeCounter].append([j, x])
+            graph[nodeCounter].insert(0,j)
+            graph[nodeCounter].insert(1,distance)
+            graph[j].insert(0,nodeCounter)
+            graph[j].insert(1,distance)            
+            
+            #graph[j].append([nodeCounter, x])
             j += 1
         nodeCounter += 1
     return graph
